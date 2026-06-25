cf714add664e561606b72032342b46cac1501d06
Maksym Masendych <maksmasenditch@gmail.com>
2026-06-25 20:58:55 +0300

    Configure GitHub Actions deployment for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/ProfiService/',
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
